67b6b878cde3bd7851131dcffed0405b887f0939
SEUZFY <zmocheng@gmail.com>
2022-09-19 15:28:31 +0200

    Basic code for read & write json file.

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include "FileIO.h"
+#include <CGAL/Polyhedron_3.h>
+
+#define DATA_PATH "/home/fengyan/geocfd/data"
+
+int main(int argc, const char** argv) {
+
+    // prompt basic info
+    std::cout << "-- activated data folder: " << DATA_PATH << '\n';
+    std::cout<<"This is: "<<argv[0]<<'\n';
+
+    //-- reading the (original)file with nlohmann json: https://github.com/nlohmann/json  
+    std::string filename = "/3dbag_v210908_fd2cee53_5907.json";
+    std::cout << "current reading file is: " << DATA_PATH + filename << '\n';
+    std::ifstream input(DATA_PATH + filename);
+    json j;
+    input >> j;
+    input.close();
+
+    // read certain building
+    FileIO fileIO;
+    std::string building_id = "NL.IMBAG.Pand.0503100000019695-0";
+    fileIO.read_certain_building(j, building_id);
+    std::cout << "number of vertices: " << fileIO.vertices.size() << '\n';
+    
+    // test output
+    /*
+    std::cout << "vertices number: " << '\n';
+    for (const auto& so : fileIO.solids)
+        for (const auto& se : so.shells)
+            for (const auto& f : se.faces)
+                for (const auto& r : f.rings)
+                {
+                    std::cout << "--------" << '\n';
+                    for (const auto& indice : r.indices)
+                        std::cout << indice << '\n';
+                }
+    */
+
+    // write file
+    std::string writeFilename = "/SimpleBuildings.json";
+    //fileIO.write_json_file(DATA_PATH + writeFilename, 0); // second argument: indicating which solid is going to be written to the file
+                    
+
+	return 0;
+
+}
